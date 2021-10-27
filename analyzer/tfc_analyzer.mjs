10ca2c13df9ec1ae78abd20b220a6672e907fd6e
root <root@vultr.guest>
2021-10-27 16:39:35 +0000

    [ING] TFC trending analysis for share.

--- analyzer/tfc_analyzer.mjs
+++ analyzer/tfc_analyzer.mjs
@@ -31,6 +31,78 @@ function dumpDiffs(total_diffs){
   fs.copyFileSync(fpath, '/root/gdrive/'+fpath)
 }
 
+function dumpDiffsForShare(total_diffs){
+
+  const rank1 = new Map([...total_diffs.entries()].sort((a, b) => b[1].get(1).df - a[1].get(1).df));
+
+  var date = new Date()
+  var str_month = '' + (date.getUTCMonth()+1)
+  if(str_month.length ==1) str_month = '0' + str_month
+  var str_day = '' + date.getUTCDate()
+  if(str_day.length ==1) str_day = '0' + str_day
+  var date_str = date.getUTCFullYear() + str_month + str_day
+
+  var fpath = 'twitter_follows_count_trend_share-' + date_str + '.txt'
+  var head = 'rank ' + 'account'.padEnd(20)
+  rank1.values().next().value.forEach(function(v,k){
+    head += (k + ' day inc').padEnd(14) + (k + ' day rel inc').padEnd(16)
+  })
+  console.log(head)
+  fs.appendFile(fpath, head, function(err){ if(err) throw err })
+
+  var i = 0
+  rank1.forEach(function(value,key){
+
+    if(i < 10){
+  	var line = '\n' + (''+(i+1)+'.').padEnd(5) + key.padEnd(20)
+	value.forEach(function(v,k){
+	    line += (''+v.df).padEnd(14) + (''+(v.rdf*100).toFixed(2)+'%').padEnd(16)
+	})
+	console.log(line)
+        fs.appendFile(fpath, line, function(err){ if(err) throw err })
+	i++
+    }
+  })
+
+  fs.copyFileSync(fpath, '/root/gdrive/'+fpath)
+}
+
+function dumpDiffsForShare2(total_diffs){
+
+  const rank1 = new Map([...total_diffs.entries()].sort((a, b) => b[1].get(1).rdf - a[1].get(1).rdf));
+
+  var date = new Date()
+  var str_month = '' + (date.getUTCMonth()+1)
+  if(str_month.length ==1) str_month = '0' + str_month
+  var str_day = '' + date.getUTCDate()
+  if(str_day.length ==1) str_day = '0' + str_day
+  var date_str = date.getUTCFullYear() + str_month + str_day
+
+  var fpath = 'twitter_follows_count_trend_share2-' + date_str + '.txt'
+  var head = 'rank ' + 'account'.padEnd(20)
+  rank1.values().next().value.forEach(function(v,k){
+    head += (k + ' day inc').padEnd(14) + (k + ' day rel inc').padEnd(16)
+  })
+  console.log(head)
+  fs.appendFile(fpath, head, function(err){ if(err) throw err })
+
+  var i = 0
+  rank1.forEach(function(value,key){
+
+    if(i < 10){
+  	var line = '\n' + (''+(i+1)+'.').padEnd(5) + key.padEnd(20)
+	value.forEach(function(v,k){
+	    line += (''+v.df).padEnd(14) + (''+(v.rdf*100).toFixed(2)+'%').padEnd(16)
+	})
+	console.log(line)
+        fs.appendFile(fpath, line, function(err){ if(err) throw err })
+	i++
+    }
+  })
+
+  fs.copyFileSync(fpath, '/root/gdrive/'+fpath)
+}
+
 export async function analyze(folder){
     var fpaths = fs.readdirSync(folder)
     var accounts = []
@@ -79,5 +151,9 @@ export async function analyze(folder){
     console.log(total_diffs)
 
     dumpDiffs(total_diffs)
+
+    dumpDiffsForShare(total_diffs)
+
+    dumpDiffsForShare2(total_diffs)
 
 }
